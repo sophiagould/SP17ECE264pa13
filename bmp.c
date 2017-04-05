b9f893c71a339e41ed1ab4e9fd22495e19cf01fb
Milind Kulkarni <milind@purdue.edu>
2017-04-05 16:48:52 -0400

    starter files

---
+++ bmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp.h"
+
+#ifndef READIMAGE_OFF
+BMPImage * readImage(FILE * fp) {
+
+// FILL IN
+
+}
+
+void freeImage(BMPImage * image) {
+	
+// FILL IN
+
+}
+#endif
+
+#ifndef WRITEIMAGE_OFF
+void writeImage(BMPImage * image, FILE * fp) {
+
+// FILL IN
+
+}
+#endif
---
+++ filter.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "filter.h"
+
+#ifndef BLUR_OFF
+BMPImage * blur(BMPImage * image) {
+
+//FILL IN	
+
+}
+#endif
+
+// EXTRA CREDIT START
+BMPImage * apply(BMPImage * image, BoxFilter f) {
+
+//FILL IN
+return NULL;
+
+}
+
+BoxFilter * readFilter(FILE * infile) {
+	
+//FILL IN
+return NULL;
+
+}
+// EXTRA CREDIT END
